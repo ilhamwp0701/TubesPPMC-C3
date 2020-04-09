834a567e06e0d4f2f82f541aa3faa64df68a9c7e
ilhamwp0701 <47767694+ilhamwp0701@users.noreply.github.com>
2020-04-09 18:09:30 +0700

    Create readFile.h

---
+++ readFile.h
@@ -0,0 +1,31 @@
+void ReadFile(int *row, int *col){
+	int i,j; //,row,col;
+	char f[20], temp[50];
+	printf("Masukkan nama file : ");
+	gets(f);
+	FILE *fp = fopen( f , "r");
+	
+	// Membaca data banyak baris
+	fgets(temp, sizeof(temp), fp);
+	sscanf(temp, "%d", *row);
+//	printf("%s\n",ans);
+//	printf("%d\n",*row);
+	
+	// Membaca data banyak kolom
+	fgets(temp, sizeof(temp), fp);
+	sscanf(temp, "%d", *col);
+//	printf("%s\n",ans);
+//	printf("%d\n",*col);
+	
+	char arr[*col][*row];
+	int count=0;
+	for(j=0; j<*row ; j++){
+		fgets(temp, sizeof(temp), fp);
+		printf("%s",temp);
+		for(i=0; i<*col ; i++){
+			arr[i][j]=temp[i];
+			count+=(arr[i][j]=='x');
+		}
+	}
+	printf("\n\n\n countt = %d", count);
+}
