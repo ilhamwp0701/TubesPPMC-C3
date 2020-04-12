c325af49313f6bfbfd67f2398ce22bffab227252
dismaswidyanto <13218065@std.stei.itb.ac.id>
2020-04-12 18:55:18 +0700

    Update parameter

--- arrayGenerator.c
+++ arrayGenerator.c
@@ -1,15 +1,15 @@
-void arrayGenerator(int row, int col ,char arr[col][row] ,FILE *fp){
-	
+void arrayGenerator(int row, int col ,char *arr ,FILE *fp){
+
 	int i,j;
-	char temp[50];
-	
+	char temp[col+2];
+
 	for(j=0; j<row ; j++){
 		fgets(temp, sizeof(temp), fp);
 		printf("%s",temp);
 		printf("j = %d\n", j);
 		for(i=0; i<col ; i++){
 			printf("i = %d\n", i);
-			arr[col][row] =temp[i];
+			*((arr+j*col) + i) =temp[i];
 //			count+=(arr[i][j]=='x');
 		}
 	}
