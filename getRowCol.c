d01d848c43c63701bd7885a75bb66ec6349e908b
ilhamwp0701 <47767694+ilhamwp0701@users.noreply.github.com>
2020-04-13 06:11:27 +0700

    Update getRowCol.c

--- getRowCol.c
+++ getRowCol.c
@@ -1,22 +1,28 @@
-void getRowCol(int *x, int *y){
+void getRowCol(int *x, int *y, FILE *fp, char *st){
+	// Variable Lokal
 	char f[20],temp[50];
 	int a, b;
 	
+	// input file
+	//	openFile(fp);
 	printf("Masukkan nama file : ");
 	gets(f);
-	FILE *fp = fopen( f , "r");
+	strcpy(st, f);
+	fp = fopen( st , "r");
 	
-	// Membaca data banyak baris
+	//// Membaca data banyak baris
 	fgets(temp, sizeof(temp), fp);
 	sscanf(temp, "%d", &a);
 //	printf("%s\n",ans);
 //	printf("%d\n",a);
 	*x=a;
 	
-	// Membaca data banyak kolom
+	//// Membaca data banyak kolom
 	fgets(temp, sizeof(temp), fp);
 	sscanf(temp, "%d", &b);
 //	printf("%s\n",ans);
 //	printf("%d\n",b);
 	*y=b;
+	
+//	char arr[col][row];
 }
