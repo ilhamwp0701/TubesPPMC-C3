980a5a32a0de1913446a1e3507b7ed86fdd688e9
dismaswidyanto <13218065@std.stei.itb.ac.id>
2020-04-12 17:17:38 +0700

    Update tick.h
    update header and function parameter

--- tick.h
+++ tick.h
@@ -1,11 +1,23 @@
-void tick(matriks,row,col){
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+ * MODUL 8 – TUGAS BESAR
+ * Kelompok         : C3
+ * Hari dan Tanggal : Minggu, 12 April 2020
+ * Asisten (NIM)    : Wuri Utami (13217024)
+ * Nama File        : tick.h
+ * Deskripsi        : Library berisi fungsi untuk menghasilkan matriks setelah 1 kali iterasi
+ *
+*/
+
+#include"cheking.h"
+
+void tick(char *matriks,int row,int col){
     int i,j;
     char nextgen[row][col];
 
     for(i=0;i<row;i++){
         for(j=0;j<col;j++){
             if(matriks[i][j]=="x"){
-                if(chek_neighbour()==2 || chek_neighbour()==3){
+                if(chek_neighbour(*matriks,i,j,row,col)==2 || chek_neighbour(*matriks,i,j,row,col)==3){
                     nextgen[i][j]="x";
                 }
                 else{
@@ -13,7 +25,7 @@ void tick(matriks,row,col){
                 }
             }
             else{
-                if(chek_neighbour()==3){
+                if(chek_neighbour(*matriks,i,j,row,col)==3){
                     nextgen[i][j]="x";
                 }
                 else{
@@ -25,7 +37,7 @@ void tick(matriks,row,col){
 
     for(i=0;i<row;i++){
         for(j=0;j<col;j++){
-            matriks[i][j]=nextgen[i][j];
+            *((matriks+i*col) + j)=nextgen[i][j];
         }
     }
 }
