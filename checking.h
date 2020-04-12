15a0d591ec36c93a4da7dec9d3f836a830112695
dismaswidyanto <13218065@std.stei.itb.ac.id>
2020-04-12 17:42:06 +0700

    Update checking.h

--- checking.h
+++ checking.h
@@ -1,7 +1,7 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
  * MODUL 8 – TUGAS BESAR
  * Kelompok         : C3
- * Hari dan Tanggal : Kamis, 9 April 2020
+ * Hari dan Tanggal : Minggu, 12 April 2020
  * Asisten (NIM)    : Wuri Utami (13217024)
  * Nama File        : checking.h
  * Deskripsi        : Library berisi fungsi untuk mengecek berapa tetangga yang hidup pada suatu matriks toroidal
