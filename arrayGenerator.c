ec692564256611090b1a691cf3f4963e42352ed4
dismaswidyanto <13218065@std.stei.itb.ac.id>
2020-04-12 18:58:39 +0700

    Update header

--- arrayGenerator.c
+++ arrayGenerator.c
@@ -1,3 +1,13 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+ * MODUL 8 – TUGAS BESAR
+ * Kelompok         : C3
+ * Hari dan Tanggal : Minggu, 12 April 2020
+ * Asisten (NIM)    : Wuri Utami (13217024)
+ * Nama File        : main.c
+ * Deskripsi        : Program untuk mengisi array dari file eksternal
+ *
+*/
+
 void arrayGenerator(int row, int col ,char *arr ,FILE *fp){
 
 	int i,j;
