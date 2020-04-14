6c575f0522a0fc44bcd9d13398166edf10ce727d
ilhamwp0701 <47767694+ilhamwp0701@users.noreply.github.com>
2020-04-14 17:43:55 +0700

    arrayGenerator FIX

--- arrayGenerator.c
+++ arrayGenerator.c
@@ -1,5 +1,5 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
- * MODUL 8 – TUGAS BESAR
+ * MODUL 8 ï¿½ TUGAS BESAR
  * Kelompok         : C3
  * Hari dan Tanggal : Minggu, 12 April 2020
  * Asisten (NIM)    : Wuri Utami (13217024)
@@ -8,19 +8,19 @@
  *
 */
 
-void arrayGenerator(int row, int col ,char *arr ,FILE *fp){
-
+void arrayGenerator(int row, int col ,char ar[col][row] ,FILE *fp, char *s){	
 	int i,j;
 	char temp[col+2];
-
+	fp = fopen( s , "r");
+	fgets(temp, sizeof(temp), fp);
+	fgets(temp, sizeof(temp), fp);
 	for(j=0; j<row ; j++){
 		fgets(temp, sizeof(temp), fp);
-		printf("%s",temp);
-		printf("j = %d\n", j);
+		// printf("%s",temp);
 		for(i=0; i<col ; i++){
-			printf("i = %d\n", i);
-			*((arr+j*col) + i) =temp[i];
-//			count+=(arr[i][j]=='x');
+			ar[i][j] =temp[i];
+			// printf("%c", ar[i][j]);
 		}
+		// printf("\n");
 	}
 }
