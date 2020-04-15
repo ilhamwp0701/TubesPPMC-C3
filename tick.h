7896999341ab1eb3797e8610e86a975797116788
dismaswidyanto <13218065@std.stei.itb.ac.id>
2020-04-15 19:41:54 +0700

    Change structure

--- tick.h
+++ tick.h
@@ -1,7 +1,7 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
- * MODUL 8 – TUGAS BESAR
+ * MODUL 8 - TUGAS BESAR
  * Kelompok         : C3
- * Hari dan Tanggal : Selasa, 14 April 2020
+ * Hari dan Tanggal : Rabu, 15 April 2020
  * Asisten (NIM)    : Wuri Utami (13217024)
  * Nama File        : tick.h
  * Deskripsi        : Library berisi fungsi untuk menghasilkan matriks setelah 1 kali iterasi
@@ -10,34 +10,4 @@
 
 //#include"checking.h"
 
-void tick(char *matriks,int row,int col){
-    int i,j;
-    char nextgen[row][col];
-
-    for(i=0;i<row;i++){
-        for(j=0;j<col;j++){
-            if(*((matriks+i*col) + j)=='X'){
-                if(checking_neighbour(matriks,i,j,row,col)==2 || checking_neighbour(matriks,i,j,row,col)==3){
-                    nextgen[i][j]='X';
-                }
-                else{
-                    nextgen[i][j]='-';
-                }
-            }
-            else{
-                if(checking_neighbour(matriks,i,j,row,col)==3){
-                    nextgen[i][j]='X';
-                }
-                else{
-                    nextgen[i][j]='-';
-                }
-            }
-        }
-    }
-
-    for(i=0;i<row;i++){
-        for(j=0;j<col;j++){
-            *((matriks+i*col) + j)=nextgen[i][j];
-        }
-    }
-}
+void tick(char *matriks,int row,int col);
