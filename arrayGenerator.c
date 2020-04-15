912de07ef6973cbd3834e092168e270c4e43fc6b
dismaswidyanto <13218065@std.stei.itb.ac.id>
2020-04-15 12:27:18 +0700

    Update how to access array

--- arrayGenerator.c
+++ arrayGenerator.c
@@ -1,14 +1,14 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
- * MODUL 8 � TUGAS BESAR
+ * MODUL 8 - TUGAS BESAR
  * Kelompok         : C3
- * Hari dan Tanggal : Minggu, 12 April 2020
+ * Hari dan Tanggal : Rabu, 15 April 2020
  * Asisten (NIM)    : Wuri Utami (13217024)
- * Nama File        : main.c
+ * Nama File        : arrayGenerator.c
  * Deskripsi        : Program untuk mengisi array dari file eksternal
  *
 */
 
-void arrayGenerator(int row, int col ,char ar[col][row] ,FILE *fp, char *s){	
+void arrayGenerator(int row, int col ,char *arr ,FILE *fp, char *s){
 	int i,j;
 	char temp[col+2];
 	fp = fopen( s , "r");
@@ -18,7 +18,7 @@ void arrayGenerator(int row, int col ,char ar[col][row] ,FILE *fp, char *s){
 		fgets(temp, sizeof(temp), fp);
 		// printf("%s",temp);
 		for(i=0; i<col ; i++){
-			ar[i][j] =temp[i];
+			*((arr+j*col) + i) =temp[i];
 			// printf("%c", ar[i][j]);
 		}
 		// printf("\n");
