364f59b4c24e48b48c3a465a54e6737f02ab54aa
dismaswidyanto <13218065@std.stei.itb.ac.id>
2020-04-16 13:02:53 +0700

    Update filename size

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
  * MODUL 8 - TUGAS BESAR
  * Kelompok         : C3
- * Hari dan Tanggal : Rabu, 15 April 2020
+ * Hari dan Tanggal : Kamis, 16 April 2020
  * Asisten (NIM)    : Wuri Utami (13217024)
  * Nama File        : main.c
  * Deskripsi        : Program untuk menggabungkan setiap fungsi dan menjalankan operasi sesuai problem yang diberikan
