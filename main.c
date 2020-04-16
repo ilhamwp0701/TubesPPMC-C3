31c68388a77c429f0f9571c8e9ad31074be0e1e0
dismaswidyanto <13218065@std.stei.itb.ac.id>
2020-04-16 13:10:50 +0700

    Update comment

--- main.c
+++ main.c
@@ -15,56 +15,57 @@
 #include<windows.h>
 #include<string.h>
 #include"display.h"
-//#include"checking.h"
 #include"tick.h"
 #include"getRowCol.h"
 #include"arrayGenerator.h"
 
 
 int main(){
-    int menu,n,i,j;
+    int menu,n,i,j;//variabel
     int max_row,max_col;
     char filename[50];
     char quit,temp;
 
 	FILE *fp = NULL;
 
-    displayJudul();
+    displayJudul();//prosedur untuk menampilkan judul game
 
-    do{
-        getRowCol(&max_row,&max_col,fp,filename);
+    do{//proses menjalankan game
+        getRowCol(&max_row,&max_col,fp,filename);//pembacaan kolom dan baris
         fclose(fp);
 
-        char currentgen[max_row][max_col];
+        char currentgen[max_row][max_col];//pembuatan matriks sesuai kolom dan baris
 
-        arrayGenerator(max_row,max_col,*currentgen,fp,filename);
+        arrayGenerator(max_row,max_col,*currentgen,fp,filename);//memasukkan seed awal ke matriks
         fclose(fp);
 
-        displaySeed(&max_row,&max_col,*currentgen);
+        displaySeed(&max_row,&max_col,*currentgen);//menampilkan seed
 
-        menu=displayMenu();
+        menu=displayMenu();//menampilkan menu
 
-        while(menu != 3){
-            if(menu==2){
-                tick(*currentgen,max_row,max_col);
-                displaySeed(&max_row,&max_col,*currentgen);
+        while(menu != 3){//proses pilihan menu
+            if(menu==2){//menu tick
+                tick(*currentgen,max_row,max_col);//proses tick
+                displaySeed(&max_row,&max_col,*currentgen);//menampilkan seed
             }
-            else if(menu==1){
-                printf("Masukkan Banyak perulangan: ");
+            else if(menu==1){//menu animate
+                printf("Masukkan Banyak perulangan: ");//memasukkan perulangan
                 scanf("%d",&n);
 
-                for(i=0;i<n;i++){
+                for(i=0;i<n;i++){//proses tick dengan n perulangan
                     system("cls");
                     tick(*currentgen,max_row,max_col);
                     displaySeed(&max_row,&max_col,*currentgen);
                     Sleep(400);
                 }
             }
-            menu=displayMenu();
+            menu=displayMenu();//menampilkan menu
         }
-        printf("Apakah anda akan memasukkan file baru? (Y/N)");
+        printf("Apakah anda akan memasukkan file baru? (Y/N)");//ingin menggunakan file seed lain atau tidak
         scanf("%c",&temp);
         scanf("%c",&quit);
         scanf("%c",&temp);
     }while(quit=='Y' || quit=='y');
+
+    return 0;
 }
--- tick.c
+++ tick.c
@@ -1,7 +1,7 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
  * MODUL 8 - TUGAS BESAR
  * Kelompok         : C3
- * Hari dan Tanggal : Rabu, 15 April 2020
+ * Hari dan Tanggal : Kamis, 16 April 2020
  * Asisten (NIM)    : Wuri Utami (13217024)
  * Nama File        : tick.c
  * Deskripsi        : Library berisi fungsi untuk menghasilkan matriks setelah 1 kali iterasi
@@ -10,22 +10,22 @@
 
 #include"checking.h"
 
-void tick(char *matriks,int row,int col){
+void tick(char *matriks,int row,int col){//fungsi tick
     int i,j;
     char nextgen[row][col];
 
-    for(i=0;i<row;i++){
+    for(i=0;i<row;i++){//proses pembacaan matriks currentgen
         for(j=0;j<col;j++){
-            if(*((matriks+i*col) + j)=='X'){
-                if(checking_neighbour(matriks,i,j,row,col)==2 || checking_neighbour(matriks,i,j,row,col)==3){
+            if(*((matriks+i*col) + j)=='X'){//cek sel hidup atau mati
+                if(checking_neighbour(matriks,i,j,row,col)==2 || checking_neighbour(matriks,i,j,row,col)==3){//cek jumlah tetangga
                     nextgen[i][j]='X';
                 }
                 else{
                     nextgen[i][j]='-';
                 }
             }
-            else{
-                if(checking_neighbour(matriks,i,j,row,col)==3){
+            else{//sel mati
+                if(checking_neighbour(matriks,i,j,row,col)==3){//cek jumlah tetangga
                     nextgen[i][j]='X';
                 }
                 else{
@@ -35,7 +35,7 @@ void tick(char *matriks,int row,int col){
         }
     }
 
-    for(i=0;i<row;i++){
+    for(i=0;i<row;i++){//proses memasukkan isi matriks nextgen ke current gen
         for(j=0;j<col;j++){
             *((matriks+i*col) + j)=nextgen[i][j];
         }
